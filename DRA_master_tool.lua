5e116b401b4ee8cc6483818338b16347917d13e7
Andrea Demetrio <andrea.demetrio@hotmail.it>
2020-10-10 23:21:08 +0200

    * added license header to script

--- DRA_master_tool.lua
+++ DRA_master_tool.lua
@@ -1,3 +1,11 @@
+  --[[Digimon Rumble Arena Training Mode Script v1.0
+	A handy Bizhawk LUA script to add some training functions to the game.
+	Code is ugly, but does what is supposed to. Feel free to improve it as you wish.
+	The code is released under a MIT license.
+	2020 - Andrea "Jens" Demetrio
+  ]]--
+
+
   -- global variables
   local trainingOverlayVisible = false
   local inputTable={}
